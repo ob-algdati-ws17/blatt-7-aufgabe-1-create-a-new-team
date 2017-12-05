21ddc8189181e20f695f1d6d0b0a35b38d4f2f95
xXxH4n5Wur5t360NoPullsxXx <gneumann@hm.edu>
2017-12-05 12:16:53 +0000

    implemented skeleton for avl tree

--- avl_tree/avl_tree.cpp
+++ avl_tree/avl_tree.cpp
@@ -1,4 +1,24 @@
 #include "avl_tree.h"
-void main() {
-	std::cout << "Hello World" << std::endl;
+
+avl-tree::avl_tree(int key) {
+}
+
+avl_tree::~avl_tree() {
+
+}
+
+typename const node search(int key) {
+	return null;
+}
+
+const bool insert(int key) {
+	return true;
+}
+
+const bool delete(int key) {
+	return true;
+}
+
+typename avl_tree::node(int key) {
+	this.key = key;
 }
--- avl_tree/avl_tree.h
+++ avl_tree/avl_tree.h
@@ -1,1 +1,24 @@
+#ifndef AVLTREE_TREE_H
+#define AVLTREE_TREE_H
+
 #include<iostream>
+
+class avl_tree {
+	private:
+		struct node {
+			node *left = nullptr;
+			node *right = nullptr;
+			int key;
+
+			node(int);
+		};
+		node *root = nullptr;
+	public:
+		avl_tree(int);
+		~avl_tree();
+		const typename avl_tree::node search(int);
+		const bool insert(int);
+		const bool remove(int);
+};
+
+#endif // AVLTREE_TREE_H
