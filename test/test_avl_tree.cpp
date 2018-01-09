ffd9b370270ea0d73f27dbf7a54755c98a596fa2
Dimitri Krivoj <dimikriv1@live.com>
2018-01-09 12:16:51 +0100

    Test removal of node with two children where left child has additional children

--- test/test_avl_tree.cpp
+++ test/test_avl_tree.cpp
@@ -278,3 +278,33 @@ TEST(AVLTreeTest, AVLTreeTest_RemoveTest_Inbetween_Left) {
     EXPECT_THAT(*d.inorder(), testing::ElementsAre(1, 69, 420, 666, 1337, 9001));
     EXPECT_THAT(*d.postorder(), testing::ElementsAre(69, 1, 666, 9001, 1337, 420));
 }
+
+
+TEST(AVLTreeTest, AVLTreeTest_RemoveTest_Inbetween_Right_More) {
+    avl_tree d;
+    EXPECT_FALSE(d.remove(1));
+    EXPECT_TRUE(d.insert(1));
+    EXPECT_FALSE(d.remove(42));
+    EXPECT_TRUE(d.insert(42));
+    EXPECT_FALSE(d.remove(69));
+    EXPECT_TRUE(d.insert(69));
+    EXPECT_FALSE(d.remove(420));
+    EXPECT_TRUE(d.insert(420));
+    EXPECT_FALSE(d.remove(666));
+    EXPECT_TRUE(d.insert(666));
+    EXPECT_FALSE(d.remove(1337));
+    EXPECT_TRUE(d.insert(1337));
+    EXPECT_FALSE(d.remove(9001));
+    EXPECT_TRUE(d.insert(9001));
+    EXPECT_FALSE(d.remove(665));
+    EXPECT_TRUE(d.insert(665));
+    EXPECT_FALSE(d.remove(667));
+    EXPECT_TRUE(d.insert(667));
+
+    EXPECT_TRUE(d.remove(1337));
+    EXPECT_FALSE(d.remove(1337));
+
+    EXPECT_THAT(*d.preorder(), testing::ElementsAre(420, 42, 1, 69, 667, 666, 665, 9001));
+    EXPECT_THAT(*d.inorder(), testing::ElementsAre(1, 42, 69, 420, 665, 666, 667, 9001));
+    EXPECT_THAT(*d.postorder(), testing::ElementsAre(1, 69, 42, 665, 666, 9001, 667, 420));
+}
