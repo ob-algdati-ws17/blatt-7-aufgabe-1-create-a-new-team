884f95489706648f772dccb97d4ba8a1369990eb
Dimitri Krivoj <dimikriv1@live.com>
2018-01-11 19:07:38 +0100

    Clean gh-pages branch

---
+++ cmake-build-debug/test/CMakeFiles/test_avl_tree.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_avl_tree.dir/main.cpp.obj"
+  "CMakeFiles/test_avl_tree.dir/test_avl_tree.cpp.obj"
+  "CMakeFiles/test_avl_tree.dir/__/avl_tree/avl_tree.cpp.obj"
+  "test_avl_tree.pdb"
+  "test_avl_tree.exe"
+  "test_avl_tree.exe.manifest"
+  "libtest_avl_tree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_avl_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
