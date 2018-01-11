6724bcd416b26bf73ee0cb807d9a31d29e19af23
Travis CI <travis@travis-ci.org>
2018-01-11 19:26:11 +0000

    Deploy code docs to GitHub Pages Travis build: 62
    Commit: 127aacc381c48e92917d3c1749523169d54f8b9d

---
+++ html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['postorder',['postorder',['../structavl__tree_1_1node.html#abe9f3981d8b35f0816657345eeada0a2',1,'avl_tree::node']]],
+  ['preorder',['preorder',['../structavl__tree_1_1node.html#a6932b4c28a69e1adc86f41f74e16cd6e',1,'avl_tree::node']]]
+];
--- html/search/search.js
+++ html/search/search.js
@@ -7,14 +7,16 @@
 
 var indexSectionsWithContent =
 {
-  0: "an",
-  1: "an"
+  0: "ainp",
+  1: "an",
+  2: "ip"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes"
+  1: "classes",
+  2: "functions"
 };
 
 function convertToId(search)
