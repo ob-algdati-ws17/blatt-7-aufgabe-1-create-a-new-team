56669121acbc45334b1a19003ff1214b4036a77c
Travis CI <travis@travis-ci.org>
2018-01-11 23:37:24 +0000

    Deploy code docs to GitHub Pages Travis build: 66
    Commit: ec34cdde5337cb2e16e115be9137e8d393585d5f

--- html/search/classes_0.js
+++ html/search/classes_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['avl_5ftree',['avl_tree',['../classavl__tree.html',1,'']]],
-  ['avltreetest',['AVLTreeTest',['../classAVLTreeTest.html',1,'']]]
+  ['avl_5ftree',['avl_tree',['../classavl__tree.html',1,'']]]
 ];
